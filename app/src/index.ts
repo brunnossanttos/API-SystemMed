a5e2f32c2d77c7231858ba54b0fd13e3766b0c55
brunnossanttos <brunossantosti99@gmail.com>
2024-03-06 01:08:39 -0300

    feat: :sparkles: develop branch

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,6 +1,6 @@
-import express, { Express } from "express";
+import express, { Express } from 'express';
 
-import routes from "./routes";
+import routes from './routes';
 
 const app: Express = express();
 
@@ -8,8 +8,8 @@ app.use(express.json());
 
 const port: number = 80;
 
-app.use("/api/v1", routes);
+app.use('/api/v1', routes);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listening on port ${port}!!!`);
 });
